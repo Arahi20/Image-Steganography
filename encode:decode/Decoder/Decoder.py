31fecfec5e5c2630b59b1517db5abde707777aad
Ahmed Rahi <ahmedrahi@pc-142-149.customer.ask4.lan>
2023-02-24 05:59:33 +0000

    The application with decode included

---
+++ encode:decode/Decoder/Decoder.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+
+def decode_image(image_path):
+    image = Image.open(image_path)
+
+    # Decode message from image
+    binary_message = ""
+    for y in range(image.height):
+        for x in range(image.width):
+            # Get RGB values of pixel
+            r, _, _ = image.getpixel((x, y))
+
+            # Get the least significant bit of the red value
+            bit = r & 0x01
+
+            # Add the bit to the binary message
+            binary_message += str(bit)
+
+    # Convert binary message to text
+    message = ""
+    for i in range(0, len(binary_message), 8):
+        byte = binary_message[i:i+8]
+        message += chr(int(byte, 2))
+
+    return message
+
+image_path=input("Please enter the filepath of the encoded image")
+print("Decoding!")
+message = decode_image(image_path)
+print(message)
---
+++ encode:decode/Encoder/Encoder.py
@@ -0,0 +1,41 @@
+from PIL import Image
+
+#Image encoder function
+image_path="/Users/ahmedrahi/myrepo/encode:decode/Encoder/image.JPEG"
+
+userinp=input("What message do you want to hide in an image")
+def encode_image(image_path, message):
+    image = Image.open(image_path)
+
+    # Convert message to binary
+    binary_message = ''.join(format(ord(i), '08b') for i in message)
+
+    # checking if the message can fit in the image comparing binary values
+    if len(binary_message) > image.width * image.height:
+        raise ValueError("Message too large for image")
+    # creating a copy of the image 
+    encoded_image = image.copy()
+    # encode message in the image O^2
+    index = 0
+    for y in range(encoded_image.height):
+        for x in range(encoded_image.width):
+            #rgb values of each pixel in the image
+            r, g, b = encoded_image.getpixel((x, y))
+
+            # next bit of the image
+            if index < len(binary_message):
+                bit = int(binary_message[index])
+            else:
+                bit = 0
+
+            # Encode the bit in the least significant bit of the red value 
+            r = (r & 0xfe) | bit
+
+            # update the pixels with new rgb values
+            encoded_image.putpixel((x, y), (r, g, b))
+            index += 1
+    encoded_image.save("/Users/ahmedrahi/myrepo/encode:decode/encoded.JPEG")
+
+encode_image(image_path,userinp)
+print("Completed succesfully!")
+
